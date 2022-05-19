389b83267a68d1c198d692e74f22b7cca25b478a
gabrielvaz <gabriel.vaz22@gmail.com>
2022-05-19 12:32:52 -0300

    introduction and string slicing

---
+++ src/main.py
@@ -0,0 +1,8 @@
+url = "bytebank.com/exchange?originCurrency=real&destinationCurrency=dolar&quantity=100"
+print(url)
+
+url_base = url[0:21]
+print(url_base)
+
+url_param = url[22:36]
+print(url_param)
